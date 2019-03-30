5c62ccf7fac231c93c15c9f6f56b5fc537b3912c
matchablue <julio.alvarenga251@myci.csuci.edu>
2019-03-30 00:09:14 -0700

    Bar Implemented as ImageView. Find local coordinates of user touch on ImageViews.

--- MusicSheet/app/src/main/java/com/example/musicsheet/MusicSheet.java
+++ MusicSheet/app/src/main/java/com/example/musicsheet/MusicSheet.java
@@ -1,25 +1,123 @@
 package com.example.musicsheet;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ScrollView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MusicSheet extends AppCompatActivity {
+
+    final int horizontal = 4;
+    final int vertical = 9;
+
+    int[][] staffPositions = new int[horizontal][vertical];
+
+    ScrollView scrollView;
+    TableLayout table;
+    ImageView imageView;
+    TextView textView;
+    TableRow row;
+
+    public String imageName;
+
+    int scrollX;
+    int scrollY;
+
+    int localX;
+    int localY;
 
     private Player player = new Player();
     Score score;
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music_sheet);
         //Toolbar toolbar = findViewById(R.id.toolbar);
         //setSupportActionBar(toolbar);
+
+        //each bar has a y position
+        //ninth position
+
+        int horizontalStart = 40;
+        int verticalStart = 130;
+        int horizontalOffset = 206/4; //hard-coded: width of imageView divided by 4 for quarter notes
+        int verticalOffset = 21; //hard-coded: eye-balled the distance between each bar lol
+
+        for(int i = 0; i < staffPositions.length; i++){ //horizontal value: 4
+            for(int j = 0; j < staffPositions[i].length; j++){//vertical value: 9
+                
+            }
+        }
+
+        scrollView = findViewById(R.id.musicSheetScroll);
+        textView = findViewById(R.id.textView);
+        table = findViewById(R.id.staffs);
+
+        for(int i = 0; i < table.getChildCount(); i++){
+            //for each row
+            row = (TableRow) table.getChildAt(i);
+            for(int j = 0; j < row.getChildCount(); j++){
+                //each imageView in row
+                imageView = (ImageView) row.getChildAt(j);
+
+                //create onTouchListener for each ImageView
+                imageView.setOnTouchListener(new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View v, MotionEvent event) {
+                        //textView.setText(v.toString());
+                        //ImageView clickedImageView = findViewById(v.getId());
+                        int imageX = (int)event.getX();
+                        int imageY = (int)event.getY();
+                        //localX = scrollX - imageX;
+                        //localY = scrollY - imageY;
+                        switch(event.getAction()){
+                            case MotionEvent.ACTION_DOWN:
+                            case MotionEvent.ACTION_POINTER_DOWN:
+                            case MotionEvent.ACTION_MOVE:
+                                textView.setText("imageX: " + imageX + " imageY: " + imageY);
+                                break;
+                        }
+
+                        //textView.setText("sX:"+ scrollX + "sY:" + scrollY + " iX:" + imageX + "iY:" + imageY + " lX:" + localX + "lY:" + localY);
+                        return false;
+                    }
+                });
+            }
+        }
+
+        /*btnNew.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ProjectsPage.class));
+            }
+        });*/
+
+        /*TableLayout tblLayout = (TableLayout)findViewById(R.id.tableLayout);
+        TableRow row = (TableRow)tblLayout.getChildAt(0); // Here get row id depending on number of row
+        Button button = (Button)row.getChildAt(XXX); // get child index on particular row
+        String buttonText = button.getText().toString();*/
+
+
+        //run this code for each view you click on
+
+
+
 
         /*goToInstrumentPanel();
         goToNotePanel();*/
@@ -37,6 +135,10 @@ public class MusicSheet extends AppCompatActivity {
             }
             score.play();
         }*/
+    }
+
+    public void AddNote(View view){
+
     }
 
     //synchronize a click on a staff to the score.tracks.addNote(time, note)
@@ -110,6 +212,36 @@ public class MusicSheet extends AppCompatActivity {
             case MotionEvent.ACTION_UP:
         }
         return false;
+
+         switch (event.getAction()){
+                    case MotionEvent.ACTION_DOWN:
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        break;
+                }
+
+                    scrollView.setOnTouchListener(new View.OnTouchListener()
+        {
+            @Override
+            public boolean onTouch(View v, MotionEvent event)
+            {
+                //set a listener for the imageViews
+                v.setOnTouchListener(new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View v, MotionEvent event) {
+                        imageName = v.toString();
+                        return false;
+                    }
+                });
+
+                //set a onTouchListener for each imageView
+                textView.setText("x:" + event.getX() + "v:" + imageName);
+
+                return false;
+            }
+        });
     }*/
 
 }
