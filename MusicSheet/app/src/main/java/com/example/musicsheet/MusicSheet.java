e33b179e64d52f81878b2dad551c7233a95ddce6
matchablue <julio.alvarenga251@myci.csuci.edu>
2019-03-30 18:33:55 -0700

    Implemented Snap function. Takes user input and returns nearest valid note location.

--- MusicSheet/app/src/main/java/com/example/musicsheet/MusicSheet.java
+++ MusicSheet/app/src/main/java/com/example/musicsheet/MusicSheet.java
@@ -32,13 +32,14 @@ public class MusicSheet extends AppCompatActivity {
     TextView textView;
     TableRow row;
 
-    public String imageName;
 
-    int scrollX;
-    int scrollY;
+    int horizontalStart = 60;
+    int horizontalMax = 720;
+    int horizontalOffset = (horizontalMax-horizontalStart)/staffPositions.length-1; //hard-coded: splits bar into 4
 
-    int localX;
-    int localY;
+    int verticalStart = 130;
+    int verticalOffset = 25; //hard-coded: eye-balled the distance between each bar lol
+    int verticalMax = 310;
 
     private Player player = new Player();
     Score score;
@@ -51,23 +52,20 @@ public class MusicSheet extends AppCompatActivity {
         //Toolbar toolbar = findViewById(R.id.toolbar);
         //setSupportActionBar(toolbar);
 
-        //each bar has a y position
-        //ninth position
-
-        int horizontalStart = 40;
-        int verticalStart = 130;
-        int horizontalOffset = 206/4; //hard-coded: width of imageView divided by 4 for quarter notes
-        int verticalOffset = 21; //hard-coded: eye-balled the distance between each bar lol
-
-        for(int i = 0; i < staffPositions.length; i++){ //horizontal value: 4
-            for(int j = 0; j < staffPositions[i].length; j++){//vertical value: 9
-                
-            }
-        }
-
         scrollView = findViewById(R.id.musicSheetScroll);
         textView = findViewById(R.id.textView);
         table = findViewById(R.id.staffs);
+
+        /*if(horizontalMax <= (horizontalStart + horizontalOffset*staffPositions.length)){
+            textView.setText("Good Vertical");
+        }
+        else{
+            textView.setText("bad");
+            horizontalMax = horizontalStart + horizontalOffset*staffPositions.length;
+        }*/
+
+        /*if(verticalMax <= (verticalStart + verticalOffset*staffPositions[0].length))
+            textView.setText("Good Vertical");*/
 
         for(int i = 0; i < table.getChildCount(); i++){
             //for each row
@@ -80,19 +78,15 @@ public class MusicSheet extends AppCompatActivity {
                 imageView.setOnTouchListener(new View.OnTouchListener() {
                     @Override
                     public boolean onTouch(View v, MotionEvent event) {
-                        //textView.setText(v.toString());
-                        //ImageView clickedImageView = findViewById(v.getId());
                         int imageX = (int)event.getX();
                         int imageY = (int)event.getY();
-                        //localX = scrollX - imageX;
-                        //localY = scrollY - imageY;
-                        switch(event.getAction()){
-                            case MotionEvent.ACTION_DOWN:
-                            case MotionEvent.ACTION_POINTER_DOWN:
-                            case MotionEvent.ACTION_MOVE:
-                                textView.setText("imageX: " + imageX + " imageY: " + imageY);
-                                break;
-                        }
+
+                        imageX = Snap(imageX, horizontalMax, horizontalStart, horizontalOffset, staffPositions.length);
+                        imageY = Snap(imageY, verticalMax, verticalStart, verticalOffset, staffPositions[0].length);
+                        if(imageX < 0)
+                            textView.setText("Touch: " + (int)event.getX());
+                        else
+                            textView.setText("imageX: " + imageX + " imageY: " + imageY);
 
                         //textView.setText("sX:"+ scrollX + "sY:" + scrollY + " iX:" + imageX + "iY:" + imageY + " lX:" + localX + "lY:" + localY);
                         return false;
@@ -153,6 +147,38 @@ public class MusicSheet extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
         player.pause();
+    }
+
+
+    //returns nearest point relative to where the user touched
+    private int Snap(int touchPoint, int max, int start, int offset, int points){
+        int prevPoint;
+        int nextPoint = 0;
+        //if not in range, snap to nearest edge
+        if(touchPoint <= start){
+            return start;
+        }
+        else if(touchPoint >= max)
+            return max;
+        else{
+            for(int i = 0; i < points-1; i++){ //horizontal positions, 4
+                prevPoint = start + (i * offset);
+                nextPoint = prevPoint + offset;
+
+                //find specific range
+                if(touchPoint >= prevPoint && touchPoint <= nextPoint){
+                    //compare distances
+                    int leftDistance = touchPoint - prevPoint;
+                    int rightDistance = nextPoint - touchPoint;
+
+                    if(leftDistance < rightDistance)
+                        return prevPoint;
+                    else
+                        return nextPoint;
+                }//end range check
+            }//end for
+            return nextPoint;
+        }
     }
 
     private void goToInstrumentPanel(){
