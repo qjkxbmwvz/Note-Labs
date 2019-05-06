53af3ebb1b71ad3d46178a5652fc43fec52a177d
talvitre <talvitre@gmail.com>
2019-05-06 13:19:37 -0700

    mainScreen button animation

--- MusicSheet/app/src/main/java/com/example/musicsheet/MainActivity.java
+++ MusicSheet/app/src/main/java/com/example/musicsheet/MainActivity.java
@@ -5,6 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -17,12 +20,34 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void openNew(View view) {
+        Button newBtn = (Button) findViewById(R.id.NewButton);
+        bounce(newBtn);
         Intent intent = new Intent(this, MusicSheet.class);
         startActivity(intent);
     }
 
     public void openExisting(View view) {
+        Button newBtn = (Button) findViewById(R.id.LoadButton);
+        bounce(newBtn);
         Intent intent = new Intent(this, FileList.class);
         startActivity(intent);
+    }
+
+    public void bounce(Button button){
+        //Handler h = new Handler();
+
+        final Animation bounce = new TranslateAnimation(0, 0, -10, 10);
+        bounce.setDuration(50);
+        bounce.setFillAfter(true);
+        bounce.setRepeatMode(Animation.REVERSE);
+        bounce.setRepeatCount(1);
+        button.startAnimation(bounce);
+
+        /*h.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        }, 150);*/
     }
 }
